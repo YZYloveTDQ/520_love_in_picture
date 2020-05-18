f1047577ab263d254c31e9d000737587954042aa
我是小杨我就这样 <913797467@qq.com>
2020-05-18 20:52:05 +0800

    Create love_520.py

---
+++ love_520.py
@@ -0,0 +1,26 @@
+from PIL import Image, ImageDraw, ImageFont
+
+font_size = 14
+text = "我老婆最美！"
+input_img_path = "wife8.jpg"
+output_img_path = "wife(8).jpg"
+
+img_raw = Image.open(input_img_path)
+img_array = img_raw.load()
+
+img_new = Image.new("RGB", img_raw.size, (0, 0, 0))
+draw = ImageDraw.Draw(img_new)
+font = ImageFont.truetype('carton.TTF', font_size)
+
+def character_generator(text):
+    while True:
+        for i in range(len(text)):
+            yield text[i]
+
+ch_gen = character_generator(text)
+
+for y in range(0, img_raw.size[1], font_size):
+    for x in range(0, img_raw.size[0], font_size):
+        draw.text((x, y), next(ch_gen), font=font, fill=img_array[x, y], direction=None)
+
+img_new.convert('RGB').save(output_img_path)
